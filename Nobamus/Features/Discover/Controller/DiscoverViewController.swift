610bc0e616f6c72ae71cc298c73fa6ca86656012
Yanislav Kononov <myryx14@gmail.com>
2017-12-17 18:47:29 +0300

    Fixed online issue

--- Nobamus/Features/Discover/Controller/DiscoverViewController.swift
+++ Nobamus/Features/Discover/Controller/DiscoverViewController.swift
@@ -52,7 +52,7 @@ class DiscoverViewController: UIViewController {
         discoverView.loginStatusLabel.isHidden = false
         automaticallyAdjustsScrollViewInsets = false
         NotificationCenter.default.addObserver(self, selector: #selector(updatePlaybackControlState(_:)), name: NSNotification.Name(rawValue: DiscoverTrackUpdatedNotificationName), object: nil)
-//        DatabaseManager.updateOnlineState(isOnline: true)
+        DatabaseManager.updateOnlineState(isOnline: true)
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
